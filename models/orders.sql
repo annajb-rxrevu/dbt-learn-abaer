92851f947d66df91654a66b92233c2ea8f6b26a1
Anna Baer <anna.baer@rxrevu.com>
2021-02-10 13:29:22 -0700

    add join condition

--- models/orders.sql
+++ models/orders.sql
@@ -2,8 +2,7 @@ select
     o.order_id,
     o.customer_id,
     sum(p.payment_amount_usd) as order_amount
-
 from {{ref('stg_orders')}} o
-left join {{ref('stg_payments')}} p
-where p.payment_status != 'fail'
+left join {{ref('stg_payments')}} p using (order_id)
+where p.payment_status = 'success'
 group by o.order_id, o.customer_id
